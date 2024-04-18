a6d0a6014a84c7392f9ae0017c023118f79d7791
verjane23 <verjane23.gmail.com>
2024-04-18 21:05:21 +0300

    react person

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,15 +1,23 @@
 /* eslint-disable no-undef */
-export const Person = ({ name, age, partner }) => (
-  <section className="Person">
-    <h2 className="Person__name">{`My name is ${name}`}</h2>
-    {age ? <p className="Person__age">{`I am ${age}`}</p> : Null}
-    {isMarried ? (
-      <p className="Person__partner">
-        {`${partner} is my {
-      person.sex === 'm' ? 'wife' : 'nusband'}`}
-      </p>
-    ) : (
-      <p className="Person__partner">I am not married</p>
-    )}
-  </section>
-);
+export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
+  let partner = 'husband';
+
+  if (sex === 'm') {
+    partner = 'wife';
+  }
+
+  return (
+    <>
+      <section className="Person">
+        <h2 className="Person__name">{`My name is ${name}`}</h2>
+        {age && <p className="Person__age">{`I am ${age}`}</p>}
+        {isMarried ? (
+          <p className="Person__partner">{`${partnerName} is my ${partner}`}</p>
+        ) : (
+          <p className="Person__partner">I am not married</p>
+        )}
+      </section>
+    </>
+  );
+};
